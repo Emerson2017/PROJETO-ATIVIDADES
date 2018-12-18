8bca6442ff9857fbb4e1d4ce3e14ffb58d6f193f
Emerson2017 <emersondima@hotmail.com>
2018-12-18 01:45:39 -0200

    iniciando com estrutura do projeto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>GERENCIAMENTO DE TAREFAS</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	</head>
+
+
+	<body>
+		<header id="header" class="">
+			<div>
+				<h2>GERENCIMANETO DE TAREFAS</h2>
+			</div>
+
+
+
+		</header>
+	</body>
+</html>
