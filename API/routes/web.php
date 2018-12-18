bd2202f0b8a08a3519e384fffaade18fa72471dc
Emerson2017 <emersondima@hotmail.com>
2018-12-18 16:26:58 -0200

    inicianndo com as rotas do sistema

--- API/routes/web.php
+++ API/routes/web.php
@@ -1,16 +1,41 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// API ROUTES
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['prefix' => 'atividade'], function(){
+	Route::get('', function(){
+		return 'TODAS AS ATIVIDADES';
+	});
+
+	Route::get('/{id}',  function($id){
+		return 'ATIVIDADE  DE ID '.$id;
+	});
+
+	
+}); 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
