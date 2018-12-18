afce2630d33e4d9a14e3abc27ca705761611f588
Emerson2017 <emersondima@hotmail.com>
2018-12-18 16:29:20 -0200

    alimentanndo pagina home

--- index.php
+++ index.php
@@ -3,17 +3,15 @@
 	<head>
 		<title>GERENCIAMENTO DE TAREFAS</title>
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/script.js"></script>
 	</head>
 
-
 	<body>
-		<header id="header" class="">
+		<header class="header">
 			<div>
 				<h2>GERENCIMANETO DE TAREFAS</h2>
 			</div>
-
-
-
 		</header>
 	</body>
 </html>
