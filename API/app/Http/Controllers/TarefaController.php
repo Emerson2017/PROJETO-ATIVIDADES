72971269d825ca67caa7e989e1a11cd8f4d3fc73
Emerson2017 <emersondima@hotmail.com>
2018-12-19 15:11:48 -0200

    metodos do controller que redirecionam as requisiçoes para a model

---
+++ API/app/Http/Controllers/TarefaController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tarefa;
+
+class TarefaController extends Controller
+{
+	protected $tarefa = null;
+
+	public function __construct(Tarefa $Tarefa){
+		$this->tarefa = $Tarefa;
+	}
+
+	public function allTarefas(){
+		return $this->tarefa->allTarefas();
+	}
+
+	public function saveTarefa(Request $request){
+		return $this->tarefa->saveTarefa($request);
+	}
+}
