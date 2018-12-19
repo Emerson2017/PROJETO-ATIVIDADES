d0f074271d1f6907314edd737137660cab222193
Emerson2017 <emersondima@hotmail.com>
2018-12-19 14:39:32 -0200

    rotas que direcionam para os respectivos metodos do controller

--- API/routes/web.php
+++ API/routes/web.php
@@ -2,15 +2,10 @@
 
 // API ROUTES
 
-Route::group(['prefix' => 'atividade'], function(){
-	Route::get('', function(){
-		return 'TODAS AS ATIVIDADES';
-	});
 
-	Route::get('/{id}',  function($id){
-		return 'ATIVIDADE  DE ID '.$id;
-	});
-
+Route::group(['prefix' => 'tarefa'], function(){
+	Route::get('', 'TarefaController@allTarefas');
+	Route::post('', 'TarefaController@saveTarefa');
 	
 }); 
  
