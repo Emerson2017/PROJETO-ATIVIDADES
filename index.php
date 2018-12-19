003b60c34bbf6cc630c28f3ab7c035956110a3e2
Emerson2017 <emersondima@hotmail.com>
2018-12-19 15:00:12 -0200

    inputs  para o cadastro de uma nova  atividade

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 	<head>
 		<title>GERENCIAMENTO DE TAREFAS</title>
+		<meta charset=utf-8>
+		<meta name=viewport content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
 		<script type="text/javascript" src="assets/js/script.js"></script>
@@ -13,5 +15,17 @@
 				<h2>GERENCIMANETO DE TAREFAS</h2>
 			</div>
 		</header>
+
+		<section>
+			<input type="text" name="name" placeholder="name" id="inputName">
+			<input type="text" name="description" placeholder="description" id="inputDescription">
+			<input type="date" name="limit_time" placeholder="limit_time" id="inputLimit">
+			<input type="text" name="priority" placeholder="priority" id="inputPriority">
+			<input type="checkbox" name="completed" id="inputCompleted">
+
+			<input type="submit" name="btn_submit" value="POST" id="btn-post">
+		</section>
+
+
 	</body>
 </html>
