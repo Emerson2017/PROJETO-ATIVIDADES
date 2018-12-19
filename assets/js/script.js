18eccbe60774444919ad0f7eb9baad7cd21f5a86
Emerson2017 <emersondima@hotmail.com>
2018-12-19 16:20:57 -0200

    Requisiçao POST para API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 
 function GetAllTarefas(){
-	$URL =  "API/public/"	
+	$URL =  "API/public/";
 	  $.ajax({
 	  method: "GET",
 	  url: $URL + "tarefa",
@@ -12,4 +12,18 @@ function GetAllTarefas(){
 	  success: function(data) {
 		ShowTarefas(data);
 	  }});
+}
+
+
+ function PostTarefa(data){
+	$URL =  "API/public/";
+	  $.ajax({
+	  method: "POST",
+	  url: $URL + "tarefa",
+	  dataType:  "JSON",
+	  data: data,
+	  success: function(data) {
+		GetAllTarefas();
+	  }
+	});
 }
