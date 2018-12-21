88122f9235fd55e28acf6e5749e728462f54f702
Emerson2017 <emersondima@hotmail.com>
2018-12-21 05:47:56 -0200

    rotas que direcionam aos metodos do controller de editar e pegar uma tarefa

--- API/routes/web.php
+++ API/routes/web.php
@@ -5,9 +5,11 @@
 
 Route::group(['prefix' => 'tarefa'], function(){
 	Route::get('', 'TarefaController@allTarefas');
+	Route::get('/{id}', 'TarefaController@getTarefa');
+
 	Route::post('', 'TarefaController@saveTarefa');
 	Route::post('/delete/{id}', 'TarefaController@deleteTarefa');
-	
+	Route::post('/edit/{id}', 'TarefaController@editTarefa');
 }); 
  
 
